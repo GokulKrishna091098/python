0b24e1922f5a8408a9f7a11c97ad61b07c7a6e80
unknown <gokulkrishnaparavur@gmail.com>
2021-01-29 13:06:53 +0530

    adding files as on 29-1-21

--- looping/primelimit.py
+++ looping/primelimit.py
@@ -1,14 +1,9 @@
 num=int(input("Enter the lower limit"))
 num1=int(input("Enter the Upper limit"))
-flag=0
-for i in range(num,num1):
-    for j in range(2,num):
-      if(num % i==0):
-        flag=1
+
+for i in range(num,num1+1):
+    for j in range(2,i):
+      if(i % j==0):
         break
-      else:
-        pass
-      if(flag==0):
-        print(num,"is prime")
-      else:
-        print(num,"is not prime")
+    else:
+        print(i)
